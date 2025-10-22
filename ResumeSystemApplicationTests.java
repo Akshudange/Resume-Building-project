0a9b0b5b06902c52da23c5062d832de36af23a03
Akshudange <106504267+Akshudange@users.noreply.github.com>
2025-10-23 01:49:29 +0530

    Add files via upload

---
+++ ResumeSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Resume_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
